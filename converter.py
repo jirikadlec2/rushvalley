321ac45665777b9ba327ae768ce0098f84bed0e4
jirikadlec2 <jirikadlec2@gmail.com>
2014-11-15 16:07:28 -0700

    check nodata values for the sensors

--- converter.py
+++ converter.py
@@ -28,12 +28,19 @@ class MPS6(Converter):
     def convert(self, response, raw_value):
         #MPS-6 water potential
         if response == 1:
+            nodata = -999999
             rw = self.port(raw_value, start_bit=0, end_bit=15)
-            return (10 ** (0.0001 * rw)) / -10.20408
+            if rw == 65535:
+                return nodata
+            else:
+                return (10 ** (0.0001 * rw)) / -10.20408
         #MPS-6 temperature
         elif response == 2:
+            nodata = -9999
             rt = self.port(raw_value, start_bit=16, end_bit=25)
-            if rt <= 900:
+            if rt == 1023:
+                return nodata
+            elif rt <= 900:
                 return float(rt - 400) / 10.0
             else:
                 return ((900 + 5 *(rt - 900)) - 400) / 10
@@ -64,29 +71,46 @@ class GS3(Converter):
 
 #SRS-Nr NDVI Field Stop (sensor #114)
 class SRSNr(Converter):
+    def get_red(self, raw_value):
+        r630 = self.port(raw_value, start_bit=1, end_bit=11)
+        if r630 > 0:
+            return (10 ** float(r630 /480.0)) / 10000.0
+        else:
+            return 0
+
+
+    def get_nir(self, raw_value):
+        r800 = self.port(raw_value, start_bit=12, end_bit=22)
+        if r800 > 0:
+            return (10 ** float(r800 / 480.0)) / 10000.0
+        else:
+            return 0
+
+
     def convert(self, response, raw_value):
         #red spectral radiance (630 nm)
         if response == 1:
-            r630 = self.port(raw_value, start_bit=1, end_bit=11)
-            return (10 ** float(r630 /480.0)) / 10000.0
+            return self.get_red(raw_value)
         #NIR spectral radiance (800 nm)
         elif response == 2:
-            r800 = self.port(raw_value, start_bit=12, end_bit=22)
-            return (10 ** float(r800 / 480.0)) / 10000.0
+            return self.get_nir(raw_value)
         #NDVI
         elif response == 3:
+            nodata = -9999
             ra = self.port(raw_value, start_bit=25, end_bit=31)
             orientation = self.port(raw_value, start_bit=23, end_bit=24)
-            alpha = 100.0 / float(ra)
-            #if the sensor returns alpha=1, use the predefined default alpha
-            default_alpha = 1.86
-            if alpha == 1:
-                alpha = default_alpha
 
-            r630 = self.port(raw_value, start_bit=1, end_bit=11)
-            red = (10 ** float(r630 / 480.0)) / 10000.0
-            r800 = self.port(raw_value, start_bit=12, end_bit=22)
-            nir = (10 ** float(r800 / 480.0)) / 10000.0
-            ndvi = float(alpha * nir - red)/float(alpha * nir + red)
-            return ndvi
+            #if the sensor returns alpha=1, use the predefined default alpha
+            #otherwise, get alpha from the measured incident radiation
+            if ra == 1:
+                alpha = 1.86
+            else:
+                alpha = 100.0 / float(ra)
+
+            red = self.get_red(raw_value)
+            nir = self.get_nir(raw_value)
+            if red > 0 and nir > 0:
+                return float(alpha * nir - red)/float(alpha * nir + red)
+            else:
+                return nodata
 
--- dxd2sql.py
+++ dxd2sql.py
@@ -6,54 +6,61 @@ from converter import Converter
 import time
 
 
-def decagon_time(raw_time):
-    return time.gmtime(raw_time + 946684800)
+class SQLManager(object):
 
-def decagon_time_local(raw_time):
-    return time.localtime(raw_time + 946684800)
+    def __init__(self, site, var, meth):
+        self.site_id = site
+        self.var_id = var
+        self.meth_id = meth
+        self.src_id = 1
+        self.qc_id = 0
 
-
-def sql_insert_values(raw_time, val, site_id, var_id, meth_id, src_id, qc_id):
-
-    utc_time = decagon_time(raw_time)
-    #TODO read the time zone from a settings file
-    local_time = decagon_time_local(raw_time)
     utc_offset = -7
 
-    sql_utc = time.strftime('%Y-%m-%d %H:%M:%S', utc_time)
-    sql_local = time.strftime('%Y-%m-%d %H:%M:%S', local_time)
 
-    return 'INSERT INTO datavalues(DataValue, UTCOffset, DateTimeUTC, LocalDateTime, \
-SiteID, VariableID, CensorCode, MethodID, SourceID, QualityControlLevelID) \
-VALUES (%s, %s, "%s", "%s", %s, %s, "nc", %s, %s, %s);' \
-           % (val, utc_offset, sql_utc, sql_local, site_id, var_id, meth_id, src_id, qc_id)
+    def decagon_time_local(self, raw_time):
+        return time.gmtime(raw_time + 946684800)
 
 
-def create_sql(dxd_file, port, sensor, response, sql_file):
-    raw_data = dxd.read_dxd(dxd_file, port)
-    #get the metadata - need to use lookup table
-    site_id = 8
-    var_id = 33
-    meth_id = 68
-    src_id = 1
-    qc_id = 0
+    def decagon_time_utc(self, raw_time):
+        return time.gmtime(raw_time + 946684800 - self.utc_offset*3600)
 
-    f = open(sql_file,'w')
 
-    nr = len(raw_data["dates"])
-    c = Converter.create(sensor)
-    for row in range(0, nr):
-        raw_time = raw_data["dates"][row]
-        raw_val = raw_data["vals"][row]
+    def insert_values(self, raw_time, val, site_id, var_id, meth_id, src_id, qc_id):
 
-        val = c.convert(response, raw_val)
-        sql = sql_insert_values(raw_time, val, site_id, var_id, meth_id, src_id, qc_id)
-        f.write(sql)
-        f.write('\n')
-    f.close()
+        utc_time = self.decagon_time_utc(raw_time)
+        local_time = self.decagon_time_local(raw_time)
+
+        sql_utc = time.strftime('%Y-%m-%d %H:%M:%S', utc_time)
+        sql_local = time.strftime('%Y-%m-%d %H:%M:%S', local_time)
+
+        return 'INSERT INTO datavalues(DataValue, UTCOffset, DateTimeUTC, LocalDateTime, \
+    SiteID, VariableID, CensorCode, MethodID, SourceID, QualityControlLevelID) \
+    VALUES (%s, %s, "%s", "%s", %s, %s, "nc", %s, %s, %s);' \
+               % (val, self.utc_offset, sql_utc, sql_local, site_id, var_id, meth_id, src_id, qc_id)
+
+
+    def create_sql(self, dxd_file, port, sensor, response, sql_file):
+        raw_data = dxd.read_dxd(dxd_file, port)
+
+        f = open(sql_file,'w')
+
+        nr = len(raw_data["dates"])
+        c = Converter.create(sensor)
+        for row in range(0, nr):
+            raw_time = raw_data["dates"][row]
+            raw_val = raw_data["vals"][row]
+
+            val = c.convert(response, raw_val)
+            sql = self.insert_values(raw_time, val, self.site_id, self.var_id,
+                                     self.meth_id, self.src_id, self.qc_id)
+            f.write(sql)
+            f.write('\n')
+        f.close()
 
 
 if __name__ == '__main__':
     dxd_file = 'C:\\jiri\\Dropbox\\BYU\\hydroinformatics\\project\\decagon_files\\5G0E3562new.dxd'
-    sql_file = 'C:\\jiri\\Dropbox\\BYU\\hydroinformatics\\project\\decagon_files\\test5.sql'
-    create_sql(dxd_file, 4, 'GS3', 3, sql_file)
+    sql_file = 'C:\\jiri\\Dropbox\\BYU\\hydroinformatics\\project\\decagon_files\\test11.sql'
+    m = SQLManager(site=8, var=33, meth=68)
+    m.create_sql(dxd_file, port=4, sensor='GS3', response=3, sql_file=sql_file)
