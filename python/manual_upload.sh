75a6be171202a45d4639951960305ec00935d943
jon <jrbelyeu@gmail.com>
2015-08-12 10:44:41 -0600

    adding script to run manual uploadfor multiple files

---
+++ python/manual_upload.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+python data_transfer.py -xls manual_files/rush_15_07_28/5G0F4358_28Jul15-1125.xls > output/5G0F4358 #no siteCodes
+python data_transfer.py -xls manual_files/rush_15_07_28/5G0F4359_28Jul15-1127.xls > output/5G0F4359 #no siteCodes
+python data_transfer.py -xls manual_files/rush_15_07_28/5G0B0491_28Jul15-1143.xls > output/5G0B0491 #no siteCodes
+python data_transfer.py -xls manual_files/rush_15_07_28/5G0E3559_28Jul15-1111.xls -v -lt "2015-06-15 00:00:00" > output/5G0E3559
+python data_transfer.py -xls manual_files/rush_15_07_28/5G0E3562_28Jul15-1141.xls -v -lt "2015-06-04 21:00:00" > output/5G0E3562
+python data_transfer.py -xls manual_files/rush_15_07_28/5G0E3563_28Jul15-1059.xls -v -lt "2015-06-13 18:00:00" > output/5G0E3563
+python data_transfer.py -xls manual_files/rush_15_07_28/5G0E3564_28Jul15-1130.xls -v -lt "2015-06-14 12:00:00" > output/5G0E3564
+python data_transfer.py -xls manual_files/rush_15_07_28/5G0E3565_28Jul15-1158.xls -v -lt "2015-06-16 00:00:00" > output/5G0E3565
+python data_transfer.py -xls manual_files/rush_15_07_28/5G0E3566_28Jul15-1145.xls -v -lt "2015-06-16 12:00:00" > output/5G0E3566
+python data_transfer.py -xls manual_files/rush_15_07_28/5G0E3574_28Jul15-1113.xls -v -lt "2015-06-16 00:00:00" > output/5G0E3574
+python data_transfer.py -xls manual_files/rush_15_07_28/5G0E3575_28Jul15-1101.xls -v -lt "2014-09-05 06:00:00" > output/5G0E3575
+python data_transfer.py -xls manual_files/rush_15_07_28/5G0E3577_28Jul15-1107.xls -v -lt "2015-06-17 00:00:00" > output/5G0E3577
+python data_transfer.py -xls manual_files/rush_15_07_28/5G0E3578_28Jul15-1154.xls -v -lt "2015-06-17 00:00:00" > output/5G0E3578
+python data_transfer.py -xls manual_files/rush_15_07_28/5G0E3580_28Jul15-1155.xls -v -lt "2015-04-30 03:00:00" > output/5G0E3580
+python data_transfer.py -xls manual_files/rush_15_07_28/5G0E3582_28Jul15-1157.xls -v -lt "2015-06-16 00:00:00" > output/5G0E3582
+python data_transfer.py -xls manual_files/rush_15_07_28/5G0E3583_28Jul15-1144.xls -v -lt "2015-06-16 12:00:00" > output/5G0E3583
+python data_transfer.py -xls manual_files/rush_15_07_28/5G0E3586_28Jul15-1142.xls -v -lt "2015-06-16 16:00:00" > output/5G0E3586
+python data_transfer.py -xls manual_files/rush_15_07_28/5G0E3704_28Jul15-1111.xls -v -lt "2015-06-17 00:00:00" > output/5G0E3704
+python data_transfer.py -xls manual_files/rush_15_07_28/5G0E3728_28Jul15-1115.xls -v -lt "2015-06-17 00:00:00" > output/5G0E3728
+python data_transfer.py -xls manual_files/rush_15_07_28/5G0E3729_28Jul15-1106.xls -v -lt "2014-10-21 09:00:00" > output/5G0E3729
+python data_transfer.py -xls manual_files/rush_15_07_28/5G0E3730_28Jul15-1130.xls -v -lt "2015-06-11 05:00:00" > output/5G0E3730
+python data_transfer.py -xls manual_files/rush_15_07_28/rushBN+_28Jul15-1128.xls > output/rushBN+ #no previous values
+python data_transfer.py -xls manual_files/rush_15_07_28/rushBNc_28Jul15-1126.xls > output/rushBNc #no previous values
